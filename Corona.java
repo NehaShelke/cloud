fb7075bffabada1d6f1b10721e31ecaa4f8a9527
Neha Shelke <62164668+NehaShelke@users.noreply.github.com>
2020-04-01 22:46:10 +0530

    simple java program

---
+++ Corona.java
@@ -0,0 +1,21 @@
+package covid;
+
+public class Corona {
+	private static final char TRUE = 0;
+
+	public static void main(String[] args) {
+		char emergency;
+		emergency = TRUE;
+		System.out.println("STAY HOME STAY SAFE");
+		
+		if(emergency==TRUE)
+		{
+			System.out.println("\n ******YOU CAN GO****** ");
+		}
+		else
+		{
+			System.out.println("FATKEEE KHAAAA :) ..........");
+		}
+	}
+
+}
